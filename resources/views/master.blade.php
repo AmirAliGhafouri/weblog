757c79d1cd5c211ad072a0ab46bfb22b5887183a
amirali <amiralighafouri80@gmail.com>
2023-12-24 10:41:10 +0330

    add bootstrap to project

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,12 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="stylesheet" type="text/css" href="{{URL::asset('boots/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{URL::asset('bootstrap-5.3.2-dist/css/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{ URL::asset('css/style.css')}}">
         <link rel="stylesheet" href="{{ URL::asset('icon/all.css')}}">
 
-        <script src="{{URL::asset('boots/js/jquery-3.6.0.min.js')}}"></script>
-        <script src="{{URL::asset('boots/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{URL::asset('bootstrap-5.3.2-dist/js/jquery-3.6.0.min.js')}}"></script>
+        <script src="{{URL::asset('bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js')}}"></script>
         <script src="{{URL::asset('icon/all.js')}}"></script>
     </head>
     <body>
