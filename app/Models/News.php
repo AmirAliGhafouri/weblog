492f4439f267a0bb694c4dfff2370b52979b25fd
amirali <amiralighafouri80@gmail.com>
2023-12-30 14:20:55 +0330

    change date_type in timestamps

--- app/Models/News.php
+++ app/Models/News.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Morilog\Jalali\Jalalian;
 
 class News extends Model
 {
@@ -19,5 +20,17 @@ class News extends Model
     public function categories()
     {
         return $this->belongsToMany(Category::class, 'category_news', 'news_id', 'category_id');
+    }
+
+    protected $dates = ['created_at', 'updated_at'];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Jalalian::fromDateTime($value)->format('Y-m-d H:i:s');
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Jalalian::fromDateTime($value)->format('Y-m-d H:i:s');
     }
  }
